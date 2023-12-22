b0b82a0a365b7168ea453c907aada53bb4cc51c8
Dimasiqqqq <142820357+Dimasiqqqq@users.noreply.github.com>
2023-12-22 12:57:16 +0200

    hw6 task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,25 +1,21 @@
 class Storage {
+  #items
   constructor(items) {
-    this._items = items;
-  }
-
-  getItems() {
-    return this._items;
-  }
-
-  addItem(newItem) {
-    this._items.push(newItem);
-  }
-
-  removeItem(itemToRemove) {
-    const index = this._items.indexOf(itemToRemove);
-    if (index !== -1) {
-      this._items.splice(index, 1);
+      this.#items = items;
     }
-  }
+getItems(){
+  return this.#items;
 }
-
+addItem(newItem) {
+  this.#items.push(newItem);
+}
+removeItem(itemToRemove){
+  this.#items = this.#items.filter(item => item !== itemToRemove);
+}
+  
+}
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
